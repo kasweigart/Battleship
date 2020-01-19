d5f616651228746fa89ed04e0d24b202bb6be8ea
Kyle Sweigart <kasweigart@gmail.com>
2020-01-19 08:34:39 -0500

    new model object

--- battleship.js
+++ battleship.js
@@ -1,16 +1,31 @@
 var view = {
-    displayArea: function(msg) {
+    displayMessage: function(msg) {
         var messageArea = document.getElementById("messageArea");
         messageArea.innerHTML = msg;
     },
+
     displayHit: function(location) {
         var cell = document.getElementById(location);
         cell.setAttribute("class", "hit");
     },
+
     displayMiss: function(location) {
         var cell = document.getElementById(location);
         cell.setAttribute("class", "miss");
     }
+};
+
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+
+    ships: [
+        {locations: ["06", "16", "26"], hits: ["","",""]},
+        {locations: ["06", "16", "26"], hits: ["","",""]},
+        {locations: ["06", "16", "26"], hits: ["","",""]}
+    ]
 };
 
 view.displayMiss("00");
@@ -19,3 +34,5 @@ view.displayMiss("55");
 view.displayHit("12");
 view.displayMiss("25");
 view.displayHit("26");
+
+view.displayMessage("Tap tap, is this thing on?");
